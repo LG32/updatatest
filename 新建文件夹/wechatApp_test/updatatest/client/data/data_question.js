a4ecc1b1e8ceb037731aee3b380185a12f9b768f
LG32 <1073684452@qq.com>
2018-05-12 21:41:09 +0800

    5/12

---
+++ 新建文件夹/wechatApp_test/updatatest/client/data/data_question.js
@@ -0,0 +1,36 @@
+/**
+ * creared by LG32 on 18/4/29.
+ */
+
+
+var question = {
+  "id": 1,
+  "data":[
+    {
+      "question_id":1,
+      "anstion_id":3,
+      "feed_source_id":23,
+      "feed_source_name":"LG32",
+      "feed_source_img":"/images/icon/icon_lg32.jpg",
+      "question": "Hello world?",
+      "simple_description":"我真是个天才",
+      "good_num":"10000+",
+      "comment_num":"1000+"
+    },{
+      "question_id": 2,
+      "anstion_id": 4,
+      "feed_source_id": 24,
+      "feed_source_name": "LG32",
+      "feed_source_img": "/images/icon/icon_lg32.jpg",
+      "question": "A good day?",
+      "simple_description": "楼上说的对",
+      "good_num": "200000+",
+      "comment_num": "20000+"
+    }
+
+  ]
+}
+
+module.exports = {
+question: question
+}
